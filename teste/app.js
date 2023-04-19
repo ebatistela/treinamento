822e292f17d5a0bec47e8e77837337d85fcc29b9
ebatistela <eibatistela@gmail.com>
2023-04-19 15:59:29 -0300

    organizando

---
+++ teste/app.js
@@ -0,0 +1,24 @@
+/*     const http = require('http');
+
+    const hostname = '127.0.0.1';
+    const port = 4004;
+
+    const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+    });
+
+    server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    }); */
+
+//node app.js
+//http://localhost:4004
+
+
+let variavel = 'teste';
+
+variavel = variavel == 'teste' ? 1 : 2;
+
+console.log(variavel)
